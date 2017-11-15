4a7f6727da19195bbcb7ed5661d9388a91d91eaa
myztajay <myztajay@hotmail.com>
2017-11-15 12:40:36 -0500

    Added Create contact form and route

---
+++ app/contacts/contacts.js
@@ -0,0 +1,18 @@
+'use strict';
+
+angular.module('myApp.contacts', ['ngRoute'])
+
+.config(['$routeProvider', function($routeProvider) {
+  $routeProvider.when('/contacts', {
+    templateUrl: 'contacts/contacts.html',
+    controller: 'ContactsCtrl'
+  });
+}])
+
+.controller('ContactsCtrl', [ '$scope', '$firebaseArray', function($scope, $firebase) {
+  var ref = firebase.database().ref();
+  $scope.contacts = $firebaseArray(ref)
+  $scope.addContact = function(){
+    console.log("added");
+  }
+}]);
