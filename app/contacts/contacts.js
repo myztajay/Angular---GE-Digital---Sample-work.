ab408e6965182c9a0c3a99b15d514a9da779ca77
myztajay <myztajay@hotmail.com>
2017-11-15 14:18:27 -0500

    Succesfully assigned contacts from database to .contact

--- app/contacts/contacts.js
+++ app/contacts/contacts.js
@@ -9,7 +9,7 @@ angular.module('myApp.contacts', ['ngRoute'])
   });
 }])
 
-.controller('ContactsCtrl', [ '$scope', function($scope) {
+.controller('ContactsCtrl', [ '$scope', '$firebaseArray', function($scope, $firebaseArray) {
   
   // just for demo purpose this database reference will be replaces to an external private file
   if (!firebase.apps.length) {
@@ -24,13 +24,24 @@ angular.module('myApp.contacts', ['ngRoute'])
     firebase.initializeApp(config);
   }
   var ref = firebase.database().ref().child('contactlist');
-  // $scope.contacts = $
+  
+  $scope.contacts = $firebaseArray(ref)
+
+   
+  
+ 
+  console.log(ref.once('value').then(function(snapshot){
+    return snapshot.val()
+  }));
+  
   $scope.addContact = function(){
-    console.log("added");
     ref.push({
       name: $scope.name,
       email: $scope.email,
       phone: $scope.phone
-    })
-  }
+    });
+    $scope.name = '';
+    $scope.email = '';
+    $scope.phone= '';
+  };
 }]);
