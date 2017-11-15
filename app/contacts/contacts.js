7dac516d7ceb22728fa2b20200b6b8d48b739ad0
myztajay <myztajay@hotmail.com>
2017-11-15 13:51:11 -0500

    Created database connection and successfully wrote to database without auth

--- app/contacts/contacts.js
+++ app/contacts/contacts.js
@@ -9,10 +9,28 @@ angular.module('myApp.contacts', ['ngRoute'])
   });
 }])
 
-.controller('ContactsCtrl', [ '$scope', '$firebaseArray', function($scope, $firebase) {
-  var ref = firebase.database().ref();
-  $scope.contacts = $firebaseArray(ref)
+.controller('ContactsCtrl', [ '$scope', function($scope) {
+  
+  // just for demo purpose this database reference will be replaces to an external private file
+  if (!firebase.apps.length) {
+    var config = {
+    apiKey: "AIzaSyBIGGuU_CsX_wY_1SVye0KVW1EJj4Ha2Vc",
+    authDomain: "contacts-angularfire.firebaseapp.com",
+    databaseURL: "https://contacts-angularfire.firebaseio.com",
+    projectId: "contacts-angularfire",
+    storageBucket: "contacts-angularfire.appspot.com",
+    messagingSenderId: "470332230879"
+    };
+    firebase.initializeApp(config);
+  }
+  var ref = firebase.database().ref().child('contactlist');
+  // $scope.contacts = $
   $scope.addContact = function(){
     console.log("added");
+    ref.push({
+      name: $scope.name,
+      email: $scope.email,
+      phone: $scope.phone
+    })
   }
 }]);
---
+++ app/myFirebase.js
@@ -0,0 +1,13 @@
+
+"use strict";
+var config = {
+  apiKey: "AIzaSyBIGGuU_CsX_wY_1SVye0KVW1EJj4Ha2Vc",
+  authDomain: "contacts-angularfire.firebaseapp.com",
+  databaseURL: "https://contacts-angularfire.firebaseio.com",
+  projectId: "contacts-angularfire",
+  storageBucket: "contacts-angularfire.appspot.com",
+  messagingSenderId: "470332230879"
+};
+module.exports = firebase.initializeApp(config);
+
+
